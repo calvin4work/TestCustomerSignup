d5a205d181ab3d06965074acd437bafdf28b6cd3
Bo Hu <bohu@deloitte.com.au>
2018-02-13 14:03:19 +1100

    fix

--- app.js
+++ app.js
@@ -2,14 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
-const client = new Client({
-  user: 'cxkztysabnllfi',
-  host: 'ec2-50-16-231-2.compute-1.amazonaws.com',
-  database: 'd7kouk9gs05vad',
-  password: '117abd374371be13841690cc497114b1e16a6b8ba585a70b4724e171c0d23a44',
-  port: 5432,
-  ssl: true
-})
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
