6bda00cab92bb090862be96d5d927aafd58e3b71
calvin4work <36460813+calvin4work@users.noreply.github.com>
2018-02-14 14:06:13 +1100

    Added transaction variables
    Added transaction variables and commented out the database part

--- api/controllers/transactionController.js
+++ api/controllers/transactionController.js
@@ -1,55 +1,53 @@
 'use strict';
 
-const { Pool, Client } = require('pg');
+//const { Pool, Client } = require('pg');
 var uuidv4 = require('uuid/v4');
 
-const client = new Client({
+/*const client = new Client({
   user: 'cxkztysabnllfi',
   host: 'ec2-50-16-231-2.compute-1.amazonaws.com',
   database: 'd7kouk9gs05vad',
   password: '117abd374371be13841690cc497114b1e16a6b8ba585a70b4724e171c0d23a44',
   port: 5432,
   ssl: true
-})
+})*/
 
-exports.create_a_customer = function(req, res) {
-	console.log('create_a_customer');	
+exports.create_a_transaction = function(req, res) {
+	console.log('create_a_transaction');	
 
   try{
 
     var receiptId = uuidv4(); // need to creat new GUID
 
     var data = req.body;
-    var id=parseInt(data.id);
-    var first_name=data.first_name;
-    var last_name=data.last_name;
-    var first_store=data.first_store;
-    var email=data.email;
-    var email_optin=parseInt(data.email_optin);
-    var email_valid=parseInt(data.email_valid);
-    var gender=parseInt(data.gender);
-    var street=data.street;
-    var address_2=data.address_2;
-    var country=data.country;
-    var zip=parseInt(data.zip);
-    var city=data.city;
-    var birthday=data.birthday;
-    var age=data.age;
-    var phone_no=data.phone_no;
-    var mob=data.mob;
-    var mob_optin=parseInt(data.mob_optin);
-    var acc_country=data.acc_country;
-    var value_tier=data.value_tier;
-    var platinum=data.platinum;
-    var language=data.language;
-    var create_date=data.create_date;
-    var club=data.club;
-    var reg_type=data.reg_type;
-    var identity_no=data.identity_no;
-    var data_source=data.data_source;
+    var cardnumber =bigInt(data.cardnumber);
+    var companycode=data.companycode;
+    var storeid=data.storeid;
+    var transactiondate=data.transactiondate;
+    var station=data.station;
+    var transactionnumber=bigInt(data.transactionnumber);
+    var seasonyear=parseInt(data.seasonyear);
+    var season=data.season;
+    var division=data.division;
+    var articlenumber=data.articlenumber;
+    var color=data.color;
+    var size=data.size;
+    var description=data.description;
+    var productclass=data.productclass;
+    var productclassdesc=data.productclassdesc;
+    var quantity=parseInt(data.quantity);
+    var price=parseFloat(data.price);
+    var costprice=parseFloat(data.costprice);
+    var fashionlevel=data.fashionlevel;
+    var pricelevel=data.pricelevel;
+    var orgsellprice=parseFloat(data.orgsellprice);
+    var netsalesbase=data.netsalesbase;
+    var netsaleslocal=parseFloat(data.netsaleslocal);
+    var coupon=data.coupon;
+    var updated=data.updated;
 
 
-    client.connect()
+/*    client.connect()
 
 
     client.query(`insert into customer(
@@ -114,7 +112,7 @@ exports.create_a_customer = function(req, res) {
       `, (err, res) => {
 
       client.end()
-    })
+    })*/
 
 
       var result = {};
@@ -128,7 +126,7 @@ exports.create_a_customer = function(req, res) {
 
     //console.log(res.rows[0])
       res.send(JSON.stringify(er));
-      client.end()
+     // client.end()
 
   }
 
